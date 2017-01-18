71a7426d46df7550e13403c3777e7b002bbdcf6c
roman <roman.rozborsky@gmail.com>
2017-01-18 04:09:22 +0200

    add logo

--- src/main/java/ua/rozborsky/tollRoadClient/classes/Window.java
+++ src/main/java/ua/rozborsky/tollRoadClient/classes/Window.java
@@ -1,11 +1,12 @@
 package ua.rozborsky.tollRoadClient.classes;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.rozborsky.tollRoadClient.interfaces.View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+
 
 /**
  * Created by roman on 18.01.2017.
@@ -24,6 +25,8 @@ public class Window implements View {
 
     private void setWindowParameters(JFrame frame) {
         setWindowSize(frame);
+        frame.setAlwaysOnTop(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     private void setWindowSize(JFrame frame) {
@@ -35,6 +38,20 @@ public class Window implements View {
     }
 
     private void setComponents(JFrame frame) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setOpaque(true);
+        panel.setBackground(new Color(145,45,45));
+        panel.add(image());
 
+        frame.add(panel);
+    }
+
+    private Component image() {
+        File logo = new File("src\\main\\resources\\image\\5.png");
+        String path = logo.getAbsolutePath();
+        JLabel label = new JLabel(new ImageIcon(path));
+
+        return label;
     }
 }
